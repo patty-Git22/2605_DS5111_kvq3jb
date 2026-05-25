f600b26aafe2e76a08a05bc276f1990487175a1a
patty-Git22 <pattysmaloon@gmail.com>
2026-05-25 18:30:59 +0000

    saving our two init files

---
+++ scripts/init_git_creds.sh
@@ -0,0 +1,9 @@
+USER=pattysmaloon@gmail.com
+NAME=patty-Git22
+
+git config --global --list
+
+git config --global user.email ${USER} 
+git config --global user.name  ${NAME} 
+
+git config --global --list
