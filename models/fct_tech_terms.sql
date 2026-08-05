9ba42a0dbee443cd1361f54c09cdf380293517fe
Patrick <pattysmaloon@gmail.com>
2026-08-04 21:11:53 -0400

    Rename TECH_TERM to TERM_NAME in SQL query

--- models/fct_tech_terms.sql
+++ models/fct_tech_terms.sql
@@ -2,7 +2,7 @@
 
 SELECT
     VIDEO_ID,
-    f.value::STRING AS TECH_TERM,
+    f.value::STRING AS TERM_NAME,
     INSERTED_AT     AS PROCESSED_AT
 FROM {{ ref('stg_youtube_transcripts') }},
 LATERAL FLATTEN(input => TECH_TERMS_ARRAY) f
